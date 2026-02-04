d0d97a0df920ad98f4c6f238ec5522483f4b0516
chunws <blorangetech@gmail.com>
2026-02-04 17:59:57 +0900

    temp measure

--- src/pages/Hanssem.jsx
+++ src/pages/Hanssem.jsx
@@ -109,7 +109,7 @@ function Hanssem() {
       {/* 메인 콘텐츠 영역 (컴포넌트 분리) */}
       {activeFilter === 'insight' && <InsightView />}
 
-      {activeFilter === 'performance' && <PerformanceView />}
+      {/* {activeFilter === 'performance' && <PerformanceView />} */}
 
       {(activeFilter === 'notice' || activeFilter === 'mypage') && (
         <main className="hanssem-main">
